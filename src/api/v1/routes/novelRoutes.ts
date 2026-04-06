64d91bcd193fc968e4dc889def8f18769d2a68f3
Keith <krobles3@rrc.ca>
2026-04-05 20:20:36 -0500

    Fixed issues and updated Routes

--- src/api/v1/routes/novelRoutes.ts
+++ src/api/v1/routes/novelRoutes.ts
@@ -1,20 +1,14 @@
 import { Router } from "express";
 import { getAllNovels, getNovelById, createNovel, updateNovel, deleteNovel } from "../controller/novelController";
-// import { validateRequest } from "../middleware/validate";
-// import { novelSchemas } from "../validation/novelSchemas";
+import { validateRequest } from "../middleware/validate";
+import { novelSchemas } from "../validation/novelSchemas";
 
 const router: Router = Router();
 
 router.get('/novels', getAllNovels);
-router.get('/novels/:id', getNovelById);
-router.post('/novels', createNovel);
-router.put('/novels/:id', updateNovel);
-router.delete('/novels/:id', deleteNovel);
-
-// router.get('/novels', getAllNovels);
-// router.get('/novels/:id', validateRequest(novelSchemas.getById), getNovelById);
-// router.post('/novels', validateRequest(novelSchemas.create), createNovel);
-// router.put('/novels/:id', validateRequest(novelSchemas.update), updateNovel);
-// router.delete('/novels/:id', validateRequest(novelSchemas.delete), deleteNovel);
+router.get('/novels/:id', validateRequest(novelSchemas.getById), getNovelById);
+router.post('/novels', validateRequest(novelSchemas.create), createNovel);
+router.put('/novels/:id', validateRequest(novelSchemas.update), updateNovel);
+router.delete('/novels/:id', validateRequest(novelSchemas.delete), deleteNovel);
 
 export default router;
